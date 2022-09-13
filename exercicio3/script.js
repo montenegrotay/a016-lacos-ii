30d21c8e06268c64d086b157170f74ee24a83755
Taynara <montenegro.taynara@gmail.com>
2022-09-13 15:57:44 -0300

    Exercícios Laços II

---
+++ exercicio3/script.js
@@ -0,0 +1,20 @@
+//Crie um array com 5 strings. Faça um programa que percorra este array e imprima as strings em formato de ranking, como no exemplo abaixo:
+
+//Utilize o for...of para resolver
+
+//entrada
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+
+//saída
+//"1 - Rússia"
+//"2 - Canadá"
+//"3 - China"
+//"4 - EUA"
+//"5 - Brasil"
+
+let rankingPaíses = 1
+
+for (let i of maioresPaises) {
+    console.log(`${rankingPaíses++} - "${i}"`)
+    
+}
